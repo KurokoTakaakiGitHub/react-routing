5e509531dde339d7c52a30bfbb2adb15ab60cd08
KurokoTakaakiGitHub <66106869+KurokoTakaakiGitHub@users.noreply.github.com>
2022-03-07 08:17:20 +0900

    ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Home />
+      <Page1 />
+      <Page2 />
     </div>
   );
 }
